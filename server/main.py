0c4a9680ba89f426da45be07bfcfdab7033e41d4
cjcon90 <cjcon90@pm.me>
2021-03-06 12:57:33 +0000

    add database count to JSON response

--- server/main.py
+++ server/main.py
@@ -4,8 +4,12 @@ import os
 import json
 from bson import json_util
 
+user = os.environ.get('DB_USER')
+password = os.environ.get('DB_PASSWORD')
+cluster = os.environ.get('DB_CLUSTER')
+collection = os.environ.get('DB_COLL')
 # Connect to MongoDB database
-client = MongoClient(f"mongodb+srv://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_CLUSTER')}.wijab.mongodb.net/{os.environ.get('DB_COLL')}?retryWrites=true&w=majority")
+client = MongoClient(f"mongodb+srv://{user}:{password}@{cluster}.wijab.mongodb.net/{collection}?retryWrites=true&w=majority")
 db = client['they_think_its_all_clover']
 coll = db['tweet']
 
@@ -19,7 +23,8 @@ def parse_json(data):
 # return as json with success code of 200
 @app.route('/tweets')
 def tweets():
-    data = (parse_json(coll.find()))
-    return jsonify(tweets=data), 200
+    total = coll.count()
+    data = (parse_json(coll.find().limit(100)))
+    return jsonify(tweets=data, total=total), 200
 
 app.run()
